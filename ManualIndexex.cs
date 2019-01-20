ecd39c924d2d6b6cfc52fb753df5032f2c67ce04
VladislavG123 <gvo_step2018@mail.ru>
2019-01-20 18:51:45 +0600

    First commit

---
+++ ManualIndexex.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexPraactick
+{
+    public class ManualIndexex
+    {
+        private int[] _data;
+        private int _size;
+        private int _firstIndex;
+
+        public ManualIndexex(int arraySize, int firstIndex)
+        {
+            _size = arraySize;
+            _firstIndex = firstIndex;
+            _data = new int[_size];
+        }
+
+        public int this[int index]
+        {
+            get { return _data[index - _firstIndex]; }
+            set { _data[index - _firstIndex] = value; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexPraactick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 4;
+            int firstIndex = -9;
+            ManualIndexex array = new ManualIndexex(size, firstIndex);
+            for (int i = 0; i < size; i++)
+            {
+                array[firstIndex + i] = i;
+            }
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine(array[firstIndex + i]);
+            }
+            Console.Read();
+        }
+    }
+}
